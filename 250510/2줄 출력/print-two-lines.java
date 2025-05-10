0539feb5c86de7cac909bf7be261900932232fd1
msj9965 <121001545+msj9965@users.noreply.github.com>
2025-05-10 15:27:54 +0900

    Title: 2줄 출력, Time: 87ms, Memory: 9MB, Status: Passed - Codetree

--- 250510/2줄 출력/print-two-lines.java
+++ 250510/2줄 출력/print-two-lines.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.our.println("World")
+        System.out.println("World");
     }
 }
