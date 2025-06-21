855e53b81029f9957d2cf08db69fde9ae5c1e820
msj9965 <121001545+msj9965@users.noreply.github.com>
2025-06-21 20:12:51 +0900

    Title: 배열의 합 계산하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250621/배열의 합 계산하기/calculate-sum-of-array.java
+++ 250621/배열의 합 계산하기/calculate-sum-of-array.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        int[]args = [3,1,4,5,9];
+    int[]args = {3,1,4,5,9};
         System.out.print(args[1]+args[3]+args[4]);
         
     }
