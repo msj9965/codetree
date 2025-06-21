fc1f4487ba419e6f306636c8e2446d6be8c634ef
msj9965 <121001545+msj9965@users.noreply.github.com>
2025-06-21 19:09:51 +0900

    Title: 문자 출력하기, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250621/문자 출력하기/print-text.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        for(int i =0; i<8; i++){
+            System.out.print(word);
+        }
+    }
+}
