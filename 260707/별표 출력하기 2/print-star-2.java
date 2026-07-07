b26e9db69da174b3b7d4e4d6ee9977606d7cfcb8
msj9965 <121001545+msj9965@users.noreply.github.com>
2026-07-07 16:11:13 +0900

    [Passed] 별표 출력하기 2 (90ms, 9MB)

---
+++ 260707/별표 출력하기 2/print-star-2.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i = n; i>0; i--){
+            for(int j = i; j>0;j--){
+                sb.append("* ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
