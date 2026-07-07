407d5b3bf46afb05e7b75a357592a2edd1a4fce0
msj9965 <121001545+msj9965@users.noreply.github.com>
2026-07-07 16:15:30 +0900

    [Passed] 직각 삼각형 출력 (95ms, 9MB)

---
+++ 260707/직각 삼각형 출력/Right-triangle-and-output.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<n;i++){
+            for(int j=0;j<2*i+1;j++){
+                sb.append("*");
+
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
